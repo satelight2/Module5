0725dccba3a9dab7ad76e321ab14ecd435b36d0f
Ngo Dinh Duy <127114092+satelight2@users.noreply.github.com>
2024-04-12 21:32:54 +0700

    ad

---
+++ demoSS2/src/test/java/com/ra/demoss2/DemoSs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ra.demoss2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSs2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
